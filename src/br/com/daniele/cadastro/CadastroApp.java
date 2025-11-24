f87bed1a9cd534dabfcb7c70964700fa738ffcc2
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-11-24 08:52:44 -0300

    interface criada

--- src/br/com/daniele/cadastro/CadastroApp.java
+++ src/br/com/daniele/cadastro/CadastroApp.java
@@ -1,5 +1,7 @@
 package br.com.daniele.cadastro;
 
+import java.time.LocalDate;
+
 public class CadastroApp {
     public static void main(String[] args) {
         System.out.println("Cadastro de Clientes");
---
+++ src/br/com/daniele/cadastro/ui/CadastroUI.java
@@ -0,0 +1,20 @@
+package br.com.daniele.cadastro.ui;
+
+public class CadastroUI {
+    public int opcao;
+
+    public void criarMenu(){
+        System.out.println("CADASTRO DE CLIENTES");
+        System.out.println("--------------------");
+        System.out.println();
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Listar todos");
+        System.out.println("3 - Exibir ciente por id");
+        System.out.println("4 - Excluir ciente");
+        System.out.println("5 - Atualizar ciente");
+        System.out.println("6 - Sair");
+        System.out.println("---------------------");
+        System.out.println();
+        System.out.print("Escolha uma opção (1-6): ");
+    }
+}
