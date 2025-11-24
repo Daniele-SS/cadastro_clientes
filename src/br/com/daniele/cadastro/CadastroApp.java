6dda6710c384a0dc701446952c75bc56d5cb01c9
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-11-24 08:55:24 -0300

    main ajustado

--- src/br/com/daniele/cadastro/CadastroApp.java
+++ src/br/com/daniele/cadastro/CadastroApp.java
@@ -1,9 +1,12 @@
 package br.com.daniele.cadastro;
 
+import br.com.daniele.cadastro.ui.CadastroUI;
+
 import java.time.LocalDate;
 
 public class CadastroApp {
     public static void main(String[] args) {
-        System.out.println("Cadastro de Clientes");
+        CadastroUI cadastroUI = new CadastroUI();
+        cadastroUI.criarMenu();
     }
 }
