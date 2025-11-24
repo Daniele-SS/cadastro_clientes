ff0fbf39480097d0e6792baba4780eda75c25a3a
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-11-24 09:53:07 -0300

    componentes adicionados no repository

--- src/br/com/daniele/cadastro/repository/ClienteRepository.java
+++ src/br/com/daniele/cadastro/repository/ClienteRepository.java
@@ -1,4 +1,27 @@
 package br.com.daniele.cadastro.repository;
 
+import br.com.daniele.cadastro.model.Cliente;
+
 public class ClienteRepository {
+    private Cliente cliente;
+
+    public void cadastrar(){
+
+    }
+
+    public void excluir(){
+
+    }
+
+    public void listar(){
+
+    }
+
+    public void exibirPorId(){
+
+    }
+
+    public void atualizar(){
+
+    }
 }
