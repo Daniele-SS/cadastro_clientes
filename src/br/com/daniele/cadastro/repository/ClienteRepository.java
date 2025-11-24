83ac89033bbb74d23a2fcbab39226b2f22143163
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-11-24 11:21:56 -0300

    Adicionado caminho do arquivo csv

--- src/br/com/daniele/cadastro/repository/ClienteRepository.java
+++ src/br/com/daniele/cadastro/repository/ClienteRepository.java
@@ -2,11 +2,25 @@ package br.com.daniele.cadastro.repository;
 
 import br.com.daniele.cadastro.model.Cliente;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
 public class ClienteRepository {
     private Cliente cliente;
 
     public void cadastrar(){
-
+        //Obter o caminho para o arquivo
+        Path arquivo = Path.of("C:\\Users\\25203622\\DS1M\\clientes\\clientes.csv");
+        try {
+            Files.writeString(arquivo, "Senai Jandira", StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("Erro ao abrir o arquivo!!");
+            System.out.println(e.getMessage());
+        }
     }
 
     public void excluir(){
